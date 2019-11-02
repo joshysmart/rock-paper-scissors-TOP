fce68311536d257326615f238264746d5af52918
Joshysmart <sanijoshua10@gmail.com>
2019-11-01 20:46:06 -0600

    A project as required by TOP

---
+++ script.js
@@ -0,0 +1,62 @@
+const rock = document.querySelector('.rock')
+const papper = document.querySelector('.papper')
+const scissors = document.querySelector('.scissors')
+const remark = document.querySelector('.remark')
+
+const userscore = document.querySelector('.userscore')
+const compscore = document.querySelector('.compscore')
+
+let compScore = 0
+let userScore = 0
+
+
+function game(e) {
+ const compChoice = compTurn()
+ const userChoice = e.target.className
+
+ //  check draw
+ if (compChoice === userChoice) {
+  remark.innerHTML = `${userChoice} covers ${compChoice} <span style="color:silver">draw</span`
+  e.target.style.border = "10px solid silver"   
+ }
+
+ // chec kwin
+ const win = userChoice === "rock" && compChoice === "scissors"
+ || userChoice === "papper" && compChoice === "rock"
+ || userChoice === "scissors" && compChoice === "papper"
+
+ if (win) {
+  remark.innerHTML = `${userChoice} beats ${compChoice} <span style="color:green">win</span`
+  userScore++
+
+  userscore.innerHTML = `${userScore < 10 ? "0" : ''}${userScore}`
+  e.target.style.border = "10px solid green"
+ }
+
+ // check loss
+ const loss = userChoice === "rock" && compChoice === "papper"
+ || userChoice === "papper" && compChoice === "scissors"
+ || userChoice === "scissors" && compChoice === "rock"
+
+ if (loss) {
+  remark.innerHTML = `${compChoice} beats ${userChoice} <span style="color:red">loss</span`
+  compScore++
+
+  compscore.innerHTML = `${compScore < 10 ? "0" : ''}${compScore}`
+  e.target.style.border = "10px solid red"
+ }
+ setTimeout(() => {
+  e.target.style.border = "none"
+ }, 500);
+}
+
+function compTurn() {
+ const compChoices = ["rock", "papper", "scissors"] 
+ const compChoice = Math.floor(Math.random() * 3)
+ const result= compChoices[compChoice]
+ return result
+}
+
+rock.addEventListener('click', game) 
+papper.addEventListener('click', game)
+scissors.addEventListener('click', game)
